c40a4a7d995a7951954181b17773abc74d30020d
David Gorski <davidgorski66@gmail.com>
2021-09-30 14:45:10 -0400

    set tolerances dynamically for integration

--- utils/solver.py
+++ utils/solver.py
@@ -1,6 +1,10 @@
 import qutip.ui.progressbar as progress_bar
 from qutip import *
 
+_atol = 1e-10
+_rtol = 1e-10
+_norm_tol = 1e-10
+
 def time_evolve(
     H_d,
     H_c, # only single control is allowed
@@ -14,7 +18,7 @@ def time_evolve(
     states_to_solve_for = []
 
     options = Options(
-        store_states=store_states, store_final_state=True,# atol=1e-10, rtol=1e-10, norm_tol=1e-10
+        store_states=store_states, store_final_state=True, atol=_atol, rtol=_rtol, norm_tol=_norm_tol
     )  # integrator will store results at each point
 
     # setup progress bar
@@ -32,3 +36,9 @@ def time_evolve(
         options=options,
         progress_bar=pbar,
     )
+
+def set_tolerances(atol=1e-10, rtol=1e-10, norm_tol=1e-10):
+    global _atol, _rtol, _norm_tol
+    _atol = atol
+    _rtol = rtol
+    _norm_tol = norm_tol
